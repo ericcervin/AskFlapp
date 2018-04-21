6cc78656dc4af379b0ad35a546413492017a38e9
Eric Ervin <ericcervin@gmail.com>
2018-04-21 17:18:55 -0400

    added robots.txt response

--- handler.py
+++ handler.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,Response
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,6 +8,10 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return "<h1>Hello, %s</h1>" % name
+
+@app.route('/robots.txt')
+def robots():
+    return Response('User-agent: *\nDisallow: /',mimetype='text/plain')
 
 if __name__ == '__main__':
     app.run(debug=True)
