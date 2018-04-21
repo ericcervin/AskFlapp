2aaf2c3563b4a93bec690ffac9b6a832733720d3
Eric Ervin <ericcervin@gmail.com>
2018-04-21 19:50:12 -0400

    added 404 response. updated root route

--- handler.py
+++ handler.py
@@ -3,7 +3,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello World!</h1>"
+    return '''<!DOCTYPE html>
+              <html lang="en">
+              <head>
+              <title>Eric Ervin Dot Com</title>
+              <style>table,th,td {
+                               border: 1px solid black;
+                               border-collapse: collapse;
+                               padding: 3px;
+                               text-align: center
+                               }
+                             td {text-align: left}</style>
+              </head>
+              <body>
+              <div id="header">
+              <h1>Eric Ervin Dot Com</h1>
+              <p>A toy website to release some Python into the world.</p>
+  
+              <p><a href="https://github.com/ericcervin/AskFlapp">https://github.com/ericcervin/AskFlapp</a></p>
+              <br>
+              </div>'''
 
 @app.route('/user/<name>')
 def user(name):
@@ -12,6 +31,16 @@ def user(name):
 @app.route('/robots.txt')
 def robots():
     return Response('User-agent: *\nDisallow: /',mimetype='text/plain')
+
+@app.errorhandler(404)
+def error404(error):
+    return '''<!DOCTYPE html>
+              <html lang="en">
+              <head>
+              <title>Error 404 Not Found</title>
+              </head>
+              <body>404 - Not Found</body>
+              </html>'''
 
 if __name__ == '__main__':
     app.run(debug=True)
